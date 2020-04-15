9196ac0a561de10486de1d3c648d63a784549a43
Vijay <vijay_g10@yahoo.com>
2020-04-16 00:49:48 +0530

    Fixed a minor issue

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ import "./assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
 import "./assets/scss/argon-dashboard-react.scss";
 
 import AdminLayout from "./layouts/Admin.jsx";
-import UserLayout from "layouts/User.jsx";
+import UserLayout from "./layouts/User.jsx";
 
 ReactDOM.render(
   <BrowserRouter>
