6325a1b480946067996f01ed474b16469a5a7deb
Ubuntu <ubuntu@ip-172-31-5-63.eu-west-3.compute.internal>
2020-04-21 18:42:08 +0000

    Enabled compression in nginx and change the base path to mojaloop

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ import AdminLayout from "./layouts/Admin.jsx";
 import UserLayout from "./layouts/User.jsx";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/mojaloop">
     <Switch>
       <Route path="/admin" render={props => <AdminLayout {...props} />} />
       <Route path="/user" render={props => <UserLayout {...props} />} />
