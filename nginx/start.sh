b1fa63251ee9758958d625717aa32f539c165dd5
Georgi Logodazhki <63578894+georgi-logodazhki@users.noreply.github.com>
2020-07-20 14:07:52 +0300

    Feature/1465 admin api security (#36)
    * add oauth and make it configurable

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,4 +1,7 @@
 if [[ ! -z "${API_BASE_URL}" ]]; then
     find /usr/share/nginx/html -type f -name "*.*" -exec sed -i -e "s|TTK_API_BASE_URL|$API_BASE_URL|g" {} \;
 fi
+if [[ ! -z "${AUTH_ENABLED}" ]]; then
+    find /usr/share/nginx/html -type f -name "*.*" -exec sed -i -e "s|TTK_AUTH_ENABLED|$AUTH_ENABLED|g" {} \;
+fi
 nginx -g "daemon off;"
