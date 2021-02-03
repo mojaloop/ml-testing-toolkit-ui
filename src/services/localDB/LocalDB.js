12af2adf47975389248b0e781d471d5368273d9a
vijayg10 <33152110+vijayg10@users.noreply.github.com>
2021-02-03 19:27:02 +0530

    Bugfix/testcase issues 3 (#83)
    * Fixed delete test case not working
    
    * Resolved an issue with file moving to folder
    
    * Added failed count in test case execution
    
    * Changed storage for folderData from localStorage to indexedDB
    
    * Fixed an issue with new test case creation
    
    * Bumped up the version

---
+++ src/services/localDB/LocalDB.js
@@ -0,0 +1,15 @@
+
+import { set, get } from 'idb-keyval';
+
+export const LocalDB = {
+
+  setItem: async function (key, value) {
+    await set(key, value);
+  },
+
+  getItem: async function (key) {
+    const data = await get(key)
+    return data
+  }
+
+};
