83b0f12907d5ac56ae3d6f129dc15981e0555473
Vijay Kumar <vijaya.guthi@modusbox.com>
2021-06-10 22:58:32 +0530

    Improved monitoring page

--- src/views/monitor/Monitor.jsx
+++ src/views/monitor/Monitor.jsx
@@ -24,153 +24,18 @@
 import React from "react";
 import socketIOClient from "socket.io-client";
 import { getConfig } from '../../utils/getConfig'
-
-import { Tag, Timeline, Card, Table, Row, Col, Button, Typography } from 'antd';
-import { ClockCircleOutlined } from '@ant-design/icons';
-
 import axios from 'axios';
 
-const { Text } = Typography
+import { Tabs } from 'antd';
 
-class IncomingTimelineItem extends React.Component {
+import ActivityLog from './ActivityLog'
+import SequenceDiagram from './SequenceDiagram'
 
-  constructor() {
-    super();
-    this.state = {
-      logsVisible: false
-    }
-  }
+const { TabPane } = Tabs;
 
-  toggleLogsVisibility = () => {
-      this.setState({logsVisible: !this.state.logsVisible})
-  }
-
-  render () {
-    const log = this.props.logs[0]
-    const info = this.props.info
-    const columns = [
-      { title: 'Message', dataIndex: 'message', key: 'message' },
-      { title: 'Log Type', dataIndex: 'verbosity', key: 'verbosity' }
-    ]
-    return (
-      <>
-        <b>{log.logTime}</b>
-        <br /><Tag color={info.erroneous ? "#f50" : "#2db7f5"} onClick={this.toggleLogsVisibility}>{info.name}</Tag>
-        <br />
-        {
-          this.state.logsVisible
-          ? (
-            <Table
-              columns={columns}
-              pagination={false}
-              expandable={{
-                expandedRowRender: log => (
-                  <>
-                  <Row>
-                    <Text strong>{log.logTime}</Text>
-                  </Row>
-                  <Row>
-                    
-                      <Text
-                        copyable = {
-                          {
-                            text: JSON.stringify(log.additionalData,null,2)
-                          }
-                        }
-                      >
-                        <pre style={{ overflow: 'scroll', 'white-space': 'pre-wrap' }}>
-                          {JSON.stringify(log.additionalData,null,2)}
-                        </pre>
-                        
-                      </Text>
-                  </Row>
-                  </>
-                ),
-                rowExpandable: log => (log.additionalData && Object.keys(log.additionalData).length !== 0),
-              }}
-              dataSource={this.props.logs.map((logItem, index) => {return {...logItem, key: index}})}
-            />
-          )
-          : null
-        }
-      </>
-    )
-  }
-}
-class IncomingTimelineSet extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      logsVisible: false
-    }
-  }
-  expandChange = (event) => {
-    event.dataItem.expanded = !event.dataItem.expanded;
-    this.forceUpdate();
-  }
-
-  toggleLogsVisibility = () => {
-      this.setState({logsVisible: !this.state.logsVisible})
-  }
-
-  getTimelineItems = () => {
-    return this.props.logSetObj.secondaryItemsArr.map(item => {
-      if (item) {
-        return (
-          <IncomingTimelineItem key={item.id} info={item} logs={this.props.logSetObj.secondaryItemsObj[item.id]} />
-        )
-      } else {
-        return (
-          <Timeline.Item dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />} color="red"><br /><br /></Timeline.Item>
-        )
-      }
-
-    })
-  }
-
-  render () {
-    const logSetObj = this.props.logSetObj
-    // const log = { logTime: 'TEMP TIME' }
-    // const info = { name: 'TEMP NAME', erroneous: false }
-    if (this.props.logSetObj.secondaryItemsArr.length > 1) {
-      return (
-        <>
-          <b>{logSetObj.logTime}</b>
-          <br /><Tag color={logSetObj.erroneous ? "#f50" : "#2db7f5"} onClick={this.toggleLogsVisibility}>{logSetObj.name}</Tag>
-          <br />
-          {
-            this.state.logsVisible
-            ? (
-                <Card>
-                  <Timeline reverse={false}>
-                    {this.getTimelineItems()}
-                  </Timeline>
-                </Card>
-            )
-            : null
-          }
-        </>
-      )
-    } else {
-      if (this.props.logSetObj.secondaryItemsArr.length === 1) {
-        const item = this.props.logSetObj.secondaryItemsArr[0]
-        return (
-          <IncomingTimelineItem key={item.id} info={item} logs={this.props.logSetObj.secondaryItemsObj[item.id]} />
-        )
-      } else {
-        return null
-      }
-    }
-  }
-}
-
-class IncomingMonitor extends React.Component {
+class Monitor extends React.Component {
 
   newState =  {
-    logs: [],
-    incomingItemsObj: {},
-    incomingItemsArr: [],
     timeline: {
       outbound: {
         socket: null,
@@ -179,12 +44,14 @@ class IncomingMonitor extends React.Component {
       inbound: {
         socket: null,
         socketTopic: "newLog"
-  }
+      }
     }
   }
 
   constructor() {
     super();
+    this.activityLogRef = React.createRef();
+    this.sequenceDiagramRef = React.createRef();
     this.state = JSON.parse(JSON.stringify(this.newState))
   }
   
@@ -200,10 +67,11 @@ class IncomingMonitor extends React.Component {
 
   componentDidMount = async () => {
     const { apiBaseUrl } = getConfig()
-    if (getConfig().isAuthEnabled && this.state.logs.length === 0) {
+    if (getConfig().isAuthEnabled) {
       const storedLogs = await axios.get(`${apiBaseUrl}/api/history/logs`)
       storedLogs.data.forEach(log => {
-        this.appendLog(log)
+        this.activityLogRef.current && this.activityLogRef.current.appendLog(log)
+        this.sequenceDiagramRef.current && this.sequenceDiagramRef.current.appendLog(log)
       })
     }
     for (const logType of Object.keys(this.state.timeline)) {
@@ -217,114 +85,27 @@ class IncomingMonitor extends React.Component {
       }
 
       item.socket.on(item.socketTopic, log => {
-        this.appendLog(log)
-      this.forceUpdate()
-    });
+        this.activityLogRef.current && this.activityLogRef.current.appendLog(log)
+        this.sequenceDiagramRef.current && this.sequenceDiagramRef.current.appendLog(log)
+        this.forceUpdate()
+      });
     }
     this.forceUpdate()
   }
 
-  appendLog = (log) => {
-    this.state.logs.push(log)
-    let primaryGroupId = 'misc'
-    if(log.uniqueId) {
-      primaryGroupId = log.uniqueId
-    }
-    // Disabling grouping by traceID temporarily, need to refactor this functionality to sync with the new inbound and outbound logs structure
-    // if(log.traceID) {
-    //   primaryGroupId = log.traceID
-    // }
-    
-
-    // Group by unique ID
-    if(!this.state.incomingItemsObj.hasOwnProperty(primaryGroupId)) {
-      this.state.incomingItemsObj[primaryGroupId] = {
-        name: '',
-        erroneous: false,
-        logTime: null,
-        secondaryItemsArr: [],
-        secondaryItemsObj: {}
-      }
-      
-      this.state.incomingItemsArr.push(primaryGroupId)
-    }
-    let primaryName = ''
-
-    const secondaryGroupId = log.uniqueId
-    if(!this.state.incomingItemsObj[primaryGroupId].secondaryItemsObj.hasOwnProperty(secondaryGroupId)) {  
-      this.state.incomingItemsObj[primaryGroupId].secondaryItemsObj[secondaryGroupId] = []
-      let name = log.message
-      if (log.resource) {
-        name = log.resource.method.toUpperCase() + ' ' + log.resource.path
-        primaryName = log.resource.method.toUpperCase() + ' > '
-      }
-      this.state.incomingItemsObj[primaryGroupId].logTime = log.logTime
-      this.state.incomingItemsObj[primaryGroupId].secondaryItemsArr.push({ id: secondaryGroupId, name, erroneous: false })
-    }
-
-    this.state.incomingItemsObj[primaryGroupId].name += primaryName + ' '
-    
-    // If the verbosity of the log is error, set the entire group as erroneous
-    if (log.verbosity === 'error') {
-      // Find the group in incomingItemsArr array
-      this.state.incomingItemsObj[primaryGroupId].erroneous = true
-      // Find the group in secondaryItemsArr array
-      const secondaryItemIndex = this.state.incomingItemsObj[primaryGroupId].secondaryItemsArr.findIndex(item => item? (item.id === secondaryGroupId) : false)
-      this.state.incomingItemsObj[primaryGroupId].secondaryItemsArr[secondaryItemIndex].erroneous = true
-    }
-
-    this.state.incomingItemsObj[primaryGroupId].position = log.notificationType === "newLog" ? "right" : "left"
-
-    this.state.incomingItemsObj[primaryGroupId].secondaryItemsObj[secondaryGroupId].push(log)
-  }
-
-  getTimelineSets = () => {
-    return this.state.incomingItemsArr.map(item => {
-      if (item) {
-        return (
-          <Timeline.Item position={this.state.incomingItemsObj[item].position}>
-            <IncomingTimelineSet key={item.id} info={item} logSetObj={this.state.incomingItemsObj[item]} />
-          </Timeline.Item>
-        )
-      } else {
-        return (
-          <Timeline.Item dot={<ClockCircleOutlined style={{ fontSize: '16px' }} />} color="red"><br /><br /></Timeline.Item>
-        )
-      }
-    })
-  }
-
-  handleClearLogs = () => {
-    this.setState(JSON.parse(JSON.stringify(this.newState)))
-  }
-
   render () {
     return (
-      <>
-      <Row>
-        <Col span={8} className="text-right" ><span className="font-weight-bold">Inbound Requests</span></Col>
-        <Col span={8} className="text-center" ><span className="font-weight-bold">|</span></Col>
-        <Col span={8} className="text-left"><span className="font-weight-bold">Outbound Requests</span>
-        <Button
-          className="float-right"
-          type="primary"
-          danger
-          onClick={this.handleClearLogs}
-        >
-          Clear
-        </Button>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={24}>
-          <Timeline mode="alternate" reverse={true} pending="Monitoring..." >
-            {this.getTimelineSets()}
-          </Timeline>
-        </Col>
-      </Row>
-      </>
+      <Tabs type="card" defaultActiveKey={'1'}>
+        <TabPane tab="Activity Log" key="1" forceRender>
+          <ActivityLog ref={this.activityLogRef} />
+        </TabPane>
+        <TabPane tab="Sequence Diagram" key="2" forceRender>
+          <SequenceDiagram ref={this.sequenceDiagramRef} />
+        </TabPane>
+      </Tabs>
+      
     )
   }
 }
 
-export default IncomingMonitor;
+export default Monitor;
