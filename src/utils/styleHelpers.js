3c798249ca357eaf638fac7dcca3eb3bb60203d7
vijayg10 <33152110+vijayg10@users.noreply.github.com>
2021-07-21 18:07:41 +0530

    Some improvements in settlements demo (#111)
    * Some improvements in settlements demo
    
    * Bumped up the version

--- src/utils/styleHelpers.js
+++ src/utils/styleHelpers.js
@@ -3,3 +3,18 @@ export const TTKColors = {
   assertionFailed: '#f50',
   assertionSkipped: '#f4c10b', // Or use #D7D700
 }
+
+export const hashRGB = (str) => {
+  // Calculage hash
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+     hash = str.charCodeAt(i) + ((hash << 12) - hash);
+  }
+
+  var c = (hash & 0xFFFFFF)
+      .toString(16)
+      .toUpperCase();
+  console.log(c, hash)
+
+  return '#' + "00000".substring(0, 6 - c.length) + c;
+}
